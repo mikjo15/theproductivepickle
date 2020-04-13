e96d61640778f88e2c5da24da0413e3928c270a1
mikjo15 <36618535+mikjo15@users.noreply.github.com>
2020-04-13 14:04:09 +1000

    Initial setup. Default files deleted. Bootstrap installed. First structure setup.

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import Jumbotron from "./components/jumbotron";
+
+function App() {
+  return <Jumbotron />
+}
+
+export default App;
