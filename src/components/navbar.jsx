34de6bdc50c1f85e417dfeb34bcdbc8477ea3825
mikjo15 <36618535+mikjo15@users.noreply.github.com>
2020-04-24 06:40:04 +1000

    Changed the navbar color

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,19 +2,19 @@ import React from "react";
 
 function Navbar() {
   return (
-    <nav className="navbar bg-white navbar-expand nav-transparent">
+    <nav className="navbar navbar-expand">
       <ul className="navbar-nav nav-fill w-100">
         <li className="nav-item">
           <a className="nav-link" href="/">BLOG<span class="sr-only">(current)</span></a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/">CV<span class="sr-only">(current)</span></a>
+          <a className="nav-link" href="/">CV</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/">PORTFOLIO<span class="sr-only">(current)</span></a>
+          <a className="nav-link" href="/">PORTFOLIO</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/">PB APP<span class="sr-only">(current)</span></a>
+          <a className="nav-link" href="/">PB APP</a>
         </li>
       </ul>
     </nav>
