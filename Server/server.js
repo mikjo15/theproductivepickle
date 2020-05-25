6ba8c8ff5b16a55c5518d008efc414fb6f5fed6b
mikjo15 <36618535+mikjo15@users.noreply.github.com>
2020-05-25 16:53:17 +1000

    Started playing around with backend, to connect front and back.

--- Server/server.js
+++ Server/server.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '/../client/public')));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, '/../client/public', 'index.html'));
+})
+
+app.post("/", (req, res) => {
+  res.send("Hey there!")
+})
 
 app.listen(port, () => {
   console.log('Server is running on port: 5000');
